94e8981575d49e7189c712e7940ada431465872f
INK0SaTOR <egorov0820@bk.ru>
2025-10-02 23:05:10 +0700

    Add files via upload

---
+++ LAB_3_TASK_3.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int sumchis(int n){
+    int ostatok = 0;
+    if (n > 9) {
+        while (n > 0){
+            ostatok += n%10;
+            n /= 10;
+        }
+        return ostatok;
+    }
+    else {
+        return n;
+    }
+}
+int main()
+{
+    int maximum = 0;
+    int minimum = 0;
+    int casech;
+    int new_num;
+    while (true){
+        cout << "Choose operation: \n 1. New number \n 2. Show max & min" << endl;
+        cin >> casech;
+        switch (casech){
+        case 1:
+            cout << "Enter number: ";
+            cin >> new_num;
+            if (sumchis(new_num) > maximum){
+                maximum = sumchis(new_num);
+            }
+            else if (sumchis(new_num) < maximum && sumchis(new_num) > minimum) {
+                minimum = sumchis(new_num);
+            }
+            break;
+        case 2:
+            cout << "Max sum = " << maximum << " min sum = " << minimum << endl;
+            return 0;
+        default:
+            cout << "invalid input" << endl;
+        }
+
+    }
+    return 0;
+}
